b1938183614a31e62b13657507ac75d4fbb0e8b2
akakou <s16003@tokyo.kosen-ac.jp>
2017-09-23 20:45:21 +0900

    [add] HTTP リクエストの作成

--- server.py
+++ server.py
@@ -11,14 +11,35 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-class ServerForProxy:
+class FlaskProxy:
     '''flask-proxyのサーバ側に関するクラス'''
     @app.route('/', defaults={'path': ''})
     @app.route('/<path:path>')
-    def server_routine(path):
+    def server_routine(self, path):
+        '''クライアントからレスポンスを送られてから、レスポンスの返答まで'''
+        response_dict = generate_client_data(requests)
         return 'response'
 
+    def generate_client_data(self, requests):
+        '''クライアントデータを生成'''
+        # HTTPリクエスト（文字列）の生成
+        header = request.method + ' ' + \
+            request.full_path + '\n' + \
+            str(request.headers)
+
+        # URLの生成
+        host = request.url_root.split('/')
+        host = host[2]
+
+        # Request辞書（後に使用）を作成
+        return request_dict = {
+            'host': client_url,
+            'port': 80,
+            'header': client_request,
+            'encode': 'utf-8'
+        }
 
 
 if __name__ == '__main__':
+    flask_proxy = FlaskProxy()
     app.run(debug=True)
